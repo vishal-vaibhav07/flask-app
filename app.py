47dab9a131e67554911a64771208d0fa3a0762d4
Vishal Vaibhav <vishalvaibhav0299@gmail.com>
2025-04-18 13:25:31 +0530

    Initial commit

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///college.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///college.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db= SQLAlchemy(app)
 #class for the database table
@@ -11,7 +12,7 @@ class College(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     desc = db.Column(db.String(500), nullable=False)
-    reg_no = db.Column(db.String(30), nullable=False)
+    reg_no = db.Column(db.String(50), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self)-> str:
@@ -27,6 +28,7 @@ def home():
         college = College(title=title, desc=desc, reg_no=reg_no)
         db.session.add(college)
         db.session.commit()
+        return redirect('/')
     allstu= College.query.all()
     print(allstu)
     return render_template('index.html', allstu=allstu)
