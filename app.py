e5392972a1f04ea56b18f1318bcd5cbf98f40fc8
Vishal Vaibhav <vishalvaibhav0299@gmail.com>
2025-04-18 14:10:15 +0530

    Fix application setup

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///college.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///college.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db= SQLAlchemy(app)
 #class for the database table
